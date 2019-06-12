10027f9809686989b18ddca2cfb7f23970566f3a
BNDong <dbnuo@foxmail.com>
2019-06-12 20:49:05 +0800

    add article info

--- src/script/base.js
+++ src/script/base.js
@@ -781,7 +781,7 @@ function Base() {
                 var tag = $(obj[i]);
                 $('#articleInfo').append('<span class="article-info-tag article-tag-class-color">'+(tag.text())+'</span>');
             });
-            bndongJs.clearIntervalTimeId(timeIds.entryTagTId);
+            bndongJs.clearIntervalTimeId(timeIds.blogPostCategoryTId);
         }
     };
 
@@ -795,7 +795,7 @@ function Base() {
                 var tag = $(obj[i]);
                 $('#articleInfo').append('<span class="article-info-tag article-tag-color">'+(tag.text())+'</span>');
             });
-            bndongJs.clearIntervalTimeId(timeIds.blogPostCategoryTId);
+            bndongJs.clearIntervalTimeId(timeIds.entryTagTId);
         }
     };
 
