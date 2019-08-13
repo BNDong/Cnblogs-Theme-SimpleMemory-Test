99f817ea612f3f5be26a0530cfa8a1055101be39
BNDong <dbnuo@foxmail.com>
2019-08-13 10:58:13 +0800

    Initialization code copy button

--- src/script/base.js
+++ src/script/base.js
@@ -905,7 +905,7 @@ function Base() {
                 cnCode   = $('div.cnblogs_code');
             cnCode.each(function (i) {
                 var obj = $(cnCode[i]);
-                if (obj.find('.cnblogs_code_toolbar').length === 0 && obj.height() > 90) {
+                if (obj.find('.cnblogs_code_toolbar').length === 0 && (obj.height() > 90 || obj.find('.code_img_closed').length > 0)) {
                     obj.append(copyHtml);
                 }
             });
