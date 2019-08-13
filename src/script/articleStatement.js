8fab8e92969cda6157d5d065b9d375039b4acb4d
BNDong <dbnuo@foxmail.com>
2019-08-13 16:18:00 +0800

    articleStatement

--- src/script/articleStatement.js
+++ src/script/articleStatement.js
@@ -38,16 +38,16 @@ $(document).ready(function(){
     var uid = fileid[0].replace('.html', ''); // 获取文章ID
 
 
-    var bottom_html = '<br><p style="font-weight:  bold;font-size: 16px;text-align:  center;color: #ddd;text-indent: 0;">__EOF__</p>';
-    bottom_html += '<p style="font-family: Microsoft YaHei,微软雅黑,MicrosoftJhengHei,华文细黑,STHeiti,MingLiu;font-size: 14px;background: hsl(220, 23%, 98%)!important;padding: 17px;border: 1px solid hsl(220, 16%, 94%)!important;border-radius: 3px;color: #414857;text-indent: 0;">';
+    var bottom_html = '<br><p class="essaySuffix-eof">__EOF__</p>';
+    bottom_html += '<p class="essaySuffix-box">';
 
 
     // =============== 二维码部分 ===============
     var imgUrl = window.cnblogsConfig.essaySuffix.codeImgUrl ? window.cnblogsConfig.essaySuffix.codeImgUrl : window.cnblogsConfig.blogAvatar;
-    bottom_html += '<div style="width: 15%; float: left;"><img src="'+imgUrl+'" style="width: 80%; max-height: 80%; border: 1px solid #ccc; padding: 5px;"/></div>';
+    bottom_html += '<div class="essaySuffix-box-left"><img src="'+imgUrl+'" alt=""/></div>';
 
     // =============== 信息部分 ===============
-    bottom_html += '<div style="width: 85%; float: right;">';
+    bottom_html += '<div class="essaySuffix-box-right">';
 
     // 设置作者和出处
     var author = '',
@@ -72,8 +72,8 @@ $(document).ready(function(){
         source  = homeUrl;
     }
 
-    bottom_html += '<span style="font-weight: bold; white-space:nowrap;">作　　者</span>：<strong><span style="font-size: 12px; color: red;">';
-    bottom_html += '<a href="'+articleSource+'" target="_blank">'+author+'</a></span></strong> <br>';
+    bottom_html += '<span class="essaySuffix-right-title">作　　者</span>：<strong><span style="font-size: 12px;">';
+    bottom_html += '<a href="'+source+'" target="_blank">'+author+'</a></span></strong> <br>';
     bottom_html += '<span style="font-weight: bold; white-space:nowrap;">出　　处</span>：<a href="'+source+'" target="_blank">'+source+'</a><br>';
 
     // 设置关于作者
@@ -81,21 +81,21 @@ $(document).ready(function(){
         ? window.cnblogsConfig.essaySuffix.aboutHtml
         : '编程路上的小学生，热爱技术，喜欢专研。评论和私信会在第一时间回复。或者<a href="http://msg.cnblogs.com/msg/send/'+tmp[3]+'" target="_blank">直接私信</a>我。';
 
-    bottom_html += '<span style="font-weight: bold; white-space:nowrap;">关于博主</span>：' + aboutHtml + '<br>';
+    bottom_html += '<span class="essaySuffix-right-title">关于博主</span>：' + aboutHtml + '<br>';
 
     // 设置版权声明
     var copyrightHtml = window.cnblogsConfig.essaySuffix.copyrightHtml
         ? window.cnblogsConfig.essaySuffix.copyrightHtml
         : '署名 - 非商业性使用 - 禁止演绎，<a href="https://creativecommons.org/licenses/by-nc-nd/4.0/" alt="协议普通文本" title="协议普通文本" target="_blank">协议普通文本</a> | <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode" alt="协议法律文本" title="协议法律文本" target="_blank">协议法律文本</a>。';
 
-    bottom_html += '<span style="font-weight: bold; white-space:nowrap;">版权声明</span>：' + copyrightHtml + '<br>';
+    bottom_html += '<span class="essaySuffix-right-title">版权声明</span>：' + copyrightHtml + '<br>';
 
     // 设置声援博主
     var supportHtml = window.cnblogsConfig.essaySuffix.supportHtml
         ? window.cnblogsConfig.essaySuffix.supportHtml
         : '如果您觉得文章对您有帮助，可以点击文章右下角<strong><span style="color: #ff0000; font-size: 12pt;">【<a id="post-up" onclick="votePost('+uid+',\'Digg\')" href="javascript:void(0);">推荐</a>】</span></strong>一下。您的鼓励是博主的最大动力！';
 
-    bottom_html += '<span style="font-weight: bold; white-space:nowrap;">声援博主</span>：' + supportHtml + '<br>';
+    bottom_html += '<span class="essaySuffix-right-title">声援博主</span>：' + supportHtml + '<br>';
 
     bottom_html += '</div>';
     bottom_html += '<div style="clear: both;"></div>';
