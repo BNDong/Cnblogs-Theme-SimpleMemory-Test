b70c241e6492ec4594727b2a1d493a1cd8e1e8e8
BNDong <dbnuo@foxmail.com>
2019-10-07 16:46:11 +0800

    test

--- src/script/articleTitle.js
+++ src/script/articleTitle.js
@@ -101,12 +101,14 @@ $(document).ready(function () {
         ) ;
     }
 
+    /**
+     * @return {string}
+     */
     function HTMLEncode(html) {
-        // var temp = document.createElement("div");
-        // (temp.textContent != null) ? (temp.textContent = html) : (temp.innerText = html);
-        // var output = temp.innerHTML;
-        // temp = null;
-        // return output;
-        return html;
+        var temp = document.createElement("div");
+        (temp.textContent != null) ? (temp.textContent = html) : (temp.innerText = html);
+        var output = temp.innerHTML;
+        temp = null;
+        return output;
     }
 });
