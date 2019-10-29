f0152d7bb1a4a19771c2f2b21f84ecb0b514c5a0
BNDong <dbnuo@foxmail.com>
2019-10-29 15:46:13 +0800

    test

--- src/script/simpleMemory.js
+++ src/script/simpleMemory.js
@@ -4,85 +4,6 @@
  * @author: BNDong, dbnuo@foxmail.com
  **/
 if (initCheck()) {
-    var sidebarHtml =
-        '<div class="container">' +
-        '    <div class="menu-wrap optiscroll" id="menuWrap" style="display:none">' +
-        '        <nav class="menu">' +
-        '            <!-- 个人简介 -->' +
-        '            <div class="introduce-box">' +
-        '                <div class="introduce-head">' +
-        '                    <div class="introduce-via" id="menuBlogAvatar"></div>' +
-        '                </div>' +
-        '                <div id="introduce"></div>' +
-        '            </div>' +
-        '            <!-- 导航 -->' +
-        '            <div class="nav-title"></div>' +
-        '            <div class="icon-list">' +
-        '                <ul id="m-nav-list">' +
-        '                </ul>' +
-        '            </div>' +
-        '            <!-- 日历 -->' +
-        '            <span id="calendar-box"></span>' +
-        '            <!-- 找找看 -->' +
-        '            <div class="m-list-title"><span>找找看</span></div>' +
-        '            <div class="m-icon-list" id="sb-sidebarSearchBox"></div>' +
-        '            <!-- 积分与排名 -->' +
-        '            <div class="m-list-title"><span>积分排名</span></div>' +
-        '            <div class="m-icon-list" id="sb-sidebarScorerank"></div>' +
-        '            <!-- 最新随笔 -->' +
-        '            <div class="m-list-title"><span>最新随笔</span></div>' +
-        '            <div class="m-icon-list" id="sb-sidebarRecentposts"></div>' +
-        '            <!-- 我的标签 -->' +
-        '            <div class="m-list-title"><span>我的标签</span></div>' +
-        '            <div class="m-icon-list" id="sb-toptags"></div>' +
-        '            <!-- 随笔分类 -->' +
-        '            <div class="m-list-title"><span>随笔分类</span></div>' +
-        '            <div class="m-icon-list" id="sb-classify"></div>' +
-        '            <!-- 随笔档案 -->' +
-        '            <div class="m-list-title"><span>随笔档案</span></div>' +
-        '            <div class="m-icon-list" id="sb-record"></div>' +
-        '            <!-- 文章档案 -->' +
-        '            <div class="m-list-title"><span>文章档案</span></div>' +
-        '            <div class="m-icon-list" id="sb-articlearchive"></div>' +
-        '            <!-- 阅读排行 -->' +
-        '            <div class="m-list-title"><span>阅读排行</span></div>' +
-        '            <div class="m-icon-list" id="sb-topview"></div>' +
-        '            <!-- 推荐排行 -->' +
-        '            <div class="m-list-title"><span>推荐排行</span></div>' +
-        '            <div class="m-icon-list" id="sb-topDiggPosts"></div>' +
-        '            <!-- 自定义列表 -->' +
-        '            <span id="menuCustomList"></span>' +
-        '        </nav>' +
-        '        <button class="close-button" id="close-button">Close Menu</button>' +
-        '        <div class="morph-shape" id="morph-shape" data-morph-open="M-7.312,0H15c0,0,66,113.339,66,399.5C81,664.006,15,800,15,800H-7.312V0z;M-7.312,0H100c0,0,0,113.839,0,400c0,264.506,0,400,0,400H-7.312V0z">' +
-        '            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 100 800" preserveAspectRatio="none">' +
-        '                <path d="M-7.312,0H0c0,0,0,113.839,0,400c0,264.506,0,400,0,400h-7.312V0z"/>' +
-        '            </svg>' +
-        '        </div>' +
-        '    </div>' +
-        '    <button class="menu-button" id="open-button">MENU</button>' +
-        '    <div class="content-wrap" id="content-wrap"></div><!-- /content-wrap -->' +
-        '</div>' +
-        '<div class="main-header">' +
-        '    <canvas id="notHomeTopCanvas"></canvas>' +
-        '    <div class="vertical">' +
-        '        <div class="main-header-content inner">' +
-        '            <link href="https://fonts.googleapis.com/css?family=Playball" rel="stylesheet">' +
-        '            <h1 class="page-title" style="font-family: \'Playball\', cursive;" id="homeTopTitle"></h1>' +
-        '            <h2 class="page-description" id="hitokoto"></h2>' +
-        '            <h3 class="page-author" id="hitokotoAuthor"></h3>' +
-        '            <h1 class="sb-title" id="sbTitle"></h1>' +
-        '            <p class="article-info" id="articleInfo"></p>' +
-        '        </div>' +
-        '    </div>' +
-        '    <a class="scroll-down" href="javascript:void(0);" data-offset="-45">' +
-        '        <span class="hidden">Scroll Down</span>' +
-        '        <i class="scroll-down-icon iconfont icon-fanhui"></i>' +
-        '    </a>' +
-        '</div>' +
-        '<div id="loading"></div>'  +
-        '<div id="bottomProgressBar"></div>' +
-        '<div id="rightMenu"></div>';
 
     window.cnblogsConfigDefault = {
         GhUserName: 'BNDong',
@@ -196,28 +117,6 @@ if (initCheck()) {
     window.cnblogsConfig = $.extend( true, window.cnblogsConfigDefault, window.cnblogsConfig );
     getVersionConfig();
 
-    // set sidebar html
-    var url = window.location.href,tmp = [];
-    tmp = url.split("/");
-    var user = tmp[3];
-    var navListHtml = '<li><a href="https://www.cnblogs.com/'+user+'/" target="_self">首页</a></li>' +
-    '<li><a href="https://msg.cnblogs.com/send/'+user+'" target="_blank">联系</a></li>' +
-    '<li><a href="https://www.cnblogs.com/'+user+'/rss" target="_blank">订阅</a></li>' +
-    '<li><a href="https://i.cnblogs.com/" target="_blank">管理</a></li>';
-
-    var menuNavList = window.cnblogsConfig.menuNavList;
-    if (menuNavList.length > 0) {
-        $.each(menuNavList, function (i) {
-            navListHtml += '<li><a href="'+(menuNavList[i][1])+'" target="_blank">'+(menuNavList[i][0])+'</a></li>';
-        });
-    }
-
-    $('#blog-news').prepend(sidebarHtml);
-    $('#m-nav-list').append(navListHtml);
-
-    // set userName
-    if (window.cnblogsConfig.blogUser === "") window.cnblogsConfig.blogUser = user;
-
     // start cache
     $.ajaxSetup({cache: true});
 
@@ -273,7 +172,11 @@ function initCheck() {
 
 // get version config
 function getVersionConfig() {
-    var url = 'https://raw.githubusercontent.com/' + window.cnblogsConfigDefault.GhUserName + '/' + window.cnblogsConfigDefault.GhRepositories + '/master/version.conf';
+
+    window.cnblogsConfigDefault.CnVersions = window.cnblogsConfigDefault.GhVersions;
+    if ( window.cnblogsConfigDefault.GhUserName !== 'BNDong') { return; }
+    // var url = 'https://raw.githubusercontent.com/' + window.cnblogsConfigDefault.GhUserName + '/' + window.cnblogsConfigDefault.GhRepositories + '/master/version.conf';
+    const url = 'https://gitee.com/dbnuo/Cnblogs-Theme-SimpleMemory/raw/master/version.conf';
 
     $.ajax({
         type: "get",
@@ -284,11 +187,7 @@ function getVersionConfig() {
         {
             var confObj = conf ? JSON.parse(conf) : false;
             var confVersion = getEndConfVal(window.cnblogsConfigDefault.GhVersions);
-            window.cnblogsConfigDefault.CnVersions = window.cnblogsConfigDefault.GhVersions;
-
-            if (confVersion) {
-                window.cnblogsConfigDefault.GhVersions = confVersion;
-            }
+            if (confVersion) { window.cnblogsConfigDefault.GhVersions = confVersion; }
 
             function getEndConfVal(thisGhVersion) {
                 var endVal = '';
