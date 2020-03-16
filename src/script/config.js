86272db35b0b6ed7ef6df31f03ba97da8eb84b13
BNDong <dbnuo@foxmail.com>
2020-03-16 16:55:30 +0800

    test

--- src/script/config.js
+++ src/script/config.js
@@ -95,7 +95,7 @@ require.config({
         base: {
             deps: [
                 'tools',
-                'css!//at.alicdn.com/t/font_543384_0tr8rjno0bqb.css', // 阿里云字体图标
+                'css!//at.alicdn.com/t/font_543384_cawzyf2xxhi.css', // 阿里云字体图标
                 'css!'+getJsDelivrUrl('google-fonts.css'), // 谷歌字体
             ]
         },
