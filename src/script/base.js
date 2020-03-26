790bb05376b27cbc89dac8f5290502223d256db9
BNDong <dbnuo@foxmail.com>
2020-03-26 17:00:17 +0800

    test

--- src/script/base.js
+++ src/script/base.js
@@ -1035,7 +1035,7 @@ function Base() {
         // 设置右下角菜单
         timeIds.setNotHomeRightMenuTId = window.setInterval( bndongJs.addNotHomeRightMenu, 1000 );
 
-        bndongJs.setCommentStyle();
+        // bndongJs.setCommentStyle();
     };
 
     /**
