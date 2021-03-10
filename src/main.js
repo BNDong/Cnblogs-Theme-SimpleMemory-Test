5553b7fa0da9a441bff92b9fcbaa2bf47c2c264c
BNDong <dbnuo@foxmail.com>
2021-03-10 17:51:32 +0800

    v2.0 重构初始化

---
+++ src/main.js
@@ -0,0 +1,15 @@
+/**
+ * UPDATES AND DOCS AT: https://github.com/BNDong
+ * https://www.cnblogs.com/bndong/
+ * @author: BNDong, dbnuo@foxmail.com
+ */
+
+import _ from 'lodash';
+// import './style/main.css';
+import config from './components/config/config';
+
+$(document).ready(function(){
+    _.__config = config();
+
+    console.log(_.__config);
+})
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const json5 = require('json5');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/main.js',
+    output: {
+        filename: 'simpleMemory.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    // devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.json5$/i,
+                type: 'json',
+                parser: {
+                    parse: json5.parse,
+                },
+            },
+        ],
+    },
+};
